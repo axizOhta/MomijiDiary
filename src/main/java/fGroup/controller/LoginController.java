c496f126510a535b6552214ce61122c0e261abc3
admin <admin@admin-HP>
2018-07-04 11:19:23 +0900

    43jsp,LoginController

---
+++ src/main/java/fGroup/controller/LoginController.java
@@ -0,0 +1,97 @@
+package fGroup.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fGroup.dao.AdminDao;
+import fGroup.dao.UsersDao;
+import fGroup.dto.Admin;
+import fGroup.dto.Users;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	UsersDao usersDao;
+
+	@Autowired
+	HttpSession session;
+
+//ユーザーログイン
+	@RequestMapping("/login")//GET
+	public String index(@ModelAttribute("form") Users users, Model model){
+		return "04login_user";
+	}
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String get(@ModelAttribute("form") Users users, Model model) {
+	String id = users.getLogin_id();
+	String pass = users.getPassword();
+
+	if(id==null && pass ==null || id.equals("") && pass.equals("")) {
+		model.addAttribute("errmsg", "ログインIDとパスワードを入力してください。");
+		System.out.println("a");
+		return "04login_user";
+	} else if ( id == null || id.equals("")) {
+		model.addAttribute("errmsg", "ログインIDを入力してください。");
+		return "04login_user";
+	} else if (pass == null || pass.equals("")) {
+		model.addAttribute("errmsg", "パスワードを入力してください。");
+		return "04login_user";
+	}
+
+	Users us = usersDao.findByIdAndPassword(id,pass);
+		if (us != null) {
+		session.setAttribute("user", us);
+			return "05mypage";
+		} else {//id、passの一致するアカウントが無い
+			model.addAttribute("errmsg", "ログインIDまたはパスワードが違います。");
+			return "04login_user";
+		}
+
+}
+
+// 管理者ログイン	//----------------------------------------------------------------------------------
+		@Autowired
+		AdminDao adminDao;
+
+		@RequestMapping("/Mlogin")//GET
+		public String index(@ModelAttribute("form") Admin admin, Model model){
+			return "35login_manager";
+		}
+
+		@RequestMapping(value="/Mlogin", method=RequestMethod.POST)
+		public String get(@ModelAttribute("form") Admin admin, Model model) {
+		Integer id = admin.getAdmin_id();
+		String pass = admin.getPassword();
+
+		if(id==null && pass ==null || id.equals("") && pass.equals("")) {
+			model.addAttribute("errmsg", "ログインIDとパスワードを入力してください。");
+			System.out.println("a");
+			return "04login_user";
+		} else if ( id == null || id.equals("")) {
+			model.addAttribute("errmsg", "ログインIDを入力してください。");
+			return "04login_user";
+		} else if (pass == null || pass.equals("")) {
+			model.addAttribute("errmsg", "パスワードを入力してください。");
+			return "04login_user";
+		}
+
+		Admin ad = adminDao.findByIdAndPassword(id,pass);
+			if (ad != null) {//ログイン
+				session.setAttribute("user", ad);
+				return "43managerMenu";
+			} else {//id、passの一致するアカウントが無い
+				model.addAttribute("errmsg", "ログインIDまたはパスワードが違います。");
+				return "35login_manager";
+			}
+		}
+
+}
+
---
+++ src/main/webapp/WEB-INF/views/43managerMenu.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>管理者メニュー画面</title>
+<script src="js/jquery.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<link href="css/f.css" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+<header>
+<div id="header-fixed">
+     <div id="a_header-bk">
+		 <div class="a_center">
+		 <div class="a_form_conf">
+            <img src="image/image002.png"  alt="" class="a_momiji"><a>もみじ日記</a>
+         </div>
+         <div class="a_menu">
+         	<ul>
+         		<li><a href="43managerMenu.html" id="manager">管理者メニュー</a></li>
+         	</ul>
+        </div>
+        </div>
+	</div>
+	</div>
+</header>
+<div class="o_">
+<div class = "o_wrapper">
+<div class="a_">
+<div class="a_btn">
+<form action="34 contactsuper">
+	<input type="submit" value="問い合わせ確認" class="a_btn_manager">
+</form>
+</div>
+<br><br>
+<div id="a_btn">
+<form action="36userinfo">
+	<input type="submit" value="ユーザー一覧" class="a_btn_manager">
+</form>
+</div>
+<br><br>
+<div id="a_btn">
+<form action="35login_manager">
+	<input type="submit" value="ログアウト" class="a_btn_manager">
+</form>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
