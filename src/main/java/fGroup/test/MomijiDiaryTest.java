041fad430d2e3c8a66d4563448306a49f4040f56
admin <admin@admin-HP>
2018-07-09 15:12:11 +0900

    Atr

---
+++ src/main/java/fGroup/test/MomijiDiaryTest.java
@@ -0,0 +1,23 @@
+package fGroup.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import fGroup.dto.Article;
+import fGroup.service.ArtService;
+
+class MomijiDiaryTest {
+
+	@Test
+	public void ArtServiceの動作確認 () throws Exception {
+
+		ArtService artservice = new ArtService();
+
+		List<Article> list = new ArrayList<>();
+
+
+	}
+
+}
