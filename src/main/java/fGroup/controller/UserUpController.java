72e2a2c620007a5e288733dd146691239ba88b8c
admin <admin@admin-HP>
2018-07-11 15:36:31 +0900

    40→39の遷移修正

--- src/main/java/fGroup/controller/UserUpController.java
+++ src/main/java/fGroup/controller/UserUpController.java
@@ -77,11 +77,11 @@ public class UserUpController {
 			return "19userinfoUpdate";
 		}
 
-		if (form.getNewEmail().hasEmailError()) {//Eメール形式じゃないとき
-			String errorMsg = messageSource.getMessage("email.error", null, Locale.getDefault());
-			model.addAttribute("msg", errorMsg);
-			return "19userinfoUpdate";
-		}
+//		if (form.getNewEmail().hasEmailError()) {//Eメール形式じゃないとき
+//			String errorMsg = messageSource.getMessage("email.error", null, Locale.getDefault());
+//			model.addAttribute("msg", errorMsg);
+//			return "19userinfoUpdate";
+//		}
 
 		if ((4 > form.getNewPassword().length())||( form.getNewPassword().length() > 10)) {//パスワードの文字数制限
 			String errorMsg = messageSource.getMessage("pass.error", null, Locale.getDefault());
